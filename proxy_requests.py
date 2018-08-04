4c34d82c55a39754b84590e3c9570b58d6a3fae9
rootVIII <colleyloyejames@gmail.com>
2018-08-04 17:25:38 -0400

    first

---
+++ proxy_requests.py
@@ -0,0 +1,41 @@
+#! /usr/bin/python3
+# https://www.roboshout.com
+import requests
+import re
+
+
+class ProxyRequests:
+    def __init__(self, url):
+        self.sockets = []
+        self.url = url
+        self.proxy = ""
+        self.request = ""
+        self.__acquire_sockets()
+
+    # get a list of US sockets from us-proxy.org
+    def __acquire_sockets(self):
+        r = requests.get("https://www.us-proxy.org/")
+        matches = re.findall(r"<td>\d+.\d+.\d+.\d+</td><td>\d+</td>", r.text)
+        revised_list = [m1.replace("<td>", "") for m1 in matches]
+        for socket_str in revised_list:
+            self.sockets.append(socket_str[:-5].replace("</td>", ":"))
+        self.__proxy_request()
+
+    # recursively try socket until successful
+    def __proxy_request(self):
+        if len(self.sockets) > 0:
+            current_socket = self.sockets.pop(0)
+            proxies = {"https": "https://" + current_socket}
+            try:
+                request = requests.get(self.url, proxies=proxies)
+                self.request = request.text
+            except:
+                self.__proxy_request()
+
+    def __str__(self):
+        return str(self.request)
+
+
+if __name__ == "__main__":
+    r = ProxyRequests("https://www.roboshout.com")
+    print(r)
