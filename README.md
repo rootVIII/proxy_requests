4e815ab4bfdbe1b2bd054d00dbf827a9d6f62e5b
rootVIII <colleyloyejames@gmail.com>
2018-08-05 14:41:13 -0400

    added HTTP POST method

--- README.md
+++ README.md
@@ -1,18 +1,27 @@
 ## make an http GET with a proxy scraped from https://www.sslproxies.org/
 <br><br>
-Pass it a fully qualified URL when inializing an instance:
-<br><br>
-&emsp;&emsp;r = ProxyRequests("https://www.roboshout.com")<br>
-&emsp;&emsp;print(r)
-
 It first scrapes proxies from the web
 <br><br>
 Then it recursively attempts to make a request if the initial request with a proxy is unsuccessful
 <br><br>
 Runs on Linux and Windows- may take a moment to run depending on current proxy
 <br><br>
+Pass it a fully qualified URL when inializing an instance:
+<br><br>
+EXAMPLE GET:<br>
+&emsp;&nbsp;r = ProxyRequests("https://www.roboshout.com")<br>
+&emsp;&nbsp;r.get()<br>
+&emsp;&nbsp;print(r)<br>
+<br><br>
+EXAMPLE POST:<br>
+&emsp;&nbsp;r = ProxyRequests("http://ptsv2.com/t/8s8j9-1533491569/post")<br>
+&emsp;&nbsp;r.post({"key": "value"}) or r.post("string here")<br>
+&emsp;&nbsp;print(r)<br>
+<br><br>
+In the URL above... Whomever made ptsv2.com (for testing HTTP requests) is awesome... NO setup and NO accounts needed
+<br><br>
 This was developed on Ubuntu 16.04.4 LTS.
 <br><br>
-POST coming soon as well as response in json format and HTTP Basic Auth
+HTTP Basic Auth coming soon as well as responses in json format etc 
 <hr>
 <b>Author: James Loye Colley  04AUG2018</b>
--- proxy_requests.py
+++ proxy_requests.py
@@ -18,10 +18,10 @@ class ProxyRequests:
         revised_list = [m1.replace("<td>", "") for m1 in matches]
         for socket_str in revised_list:
             self.sockets.append(socket_str[:-5].replace("</td>", ":"))
-        self.__proxy_request()
+
 
     # recursively try socket until successful
-    def __proxy_request(self):
+    def get(self):
         if len(self.sockets) > 0:
             current_socket = self.sockets.pop(0)
             proxies = {"https": "https://" + current_socket}
@@ -29,14 +29,34 @@ class ProxyRequests:
                 request = requests.get(self.url, proxies=proxies)
                 self.request = request.text
             except:
-                print('hit next recursion level')
-                self.__proxy_request()
+                print('working...')
+                self.get()
 
     def __str__(self):
         return str(self.request)
 
+    # recursively try socket until successful POST
+    def post(self, data):
+        if len(self.sockets) > 0:
+            current_socket = self.sockets.pop(0)
+            proxies = {"https": "https://" + current_socket}
+            try:
+                request = requests.post(self.url, data=data, proxies=proxies)
+                self.request = request.text
+            except:
+                print('working...')
+                self.post(self.url)
+
 
 if __name__ == "__main__":
-    r = ProxyRequests("https://www.roboshout.com")
+    # example GET
+    #r = ProxyRequests("http://www.roboshout.com")
+    #r.get()
+    #print(r)
+
+    # example POST
+    r = ProxyRequests("http://ptsv2.com/t/8s8j9-1533491569/post")
+    # can pass a string, dict:
+    #r.post("dump goes in toilet")
+    r.post({"key": "value", "one": "two"})
     print(r)
-    # r = ProxyRequests.get("https://www.roboshout.com")
