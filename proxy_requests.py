3273c40f77d49000670f0bfab76faf8766abe524
rootVIII <colleyloyejames@gmail.com>
2020-01-21 21:34:11 -0500

    Version: 0.3.6, raise timeout level, better error reporting if exception is not a timeout or proxy error

--- proxy_requests.py
+++ proxy_requests.py
@@ -1,6 +1,6 @@
-#! /usr/bin/python3
 import requests
 from re import findall
+# rootVIII
 
 
 class ProxyRequests:
@@ -9,9 +9,11 @@ class ProxyRequests:
         self.url = url
         self.request, self.proxy = '', ''
         self.proxy_used, self.raw_content = '', ''
-        self.status_code, self.try_count = 0, 15
+        self.status_code = 0
         self.headers, self.file_dict = {}, {}
         self.json = None
+        self.timeout = 8.0
+        self.errs = ('ConnectTimeout', 'ProxyError')
         self.acquire_sockets()
 
     # get a list of sockets from sslproxies.org
@@ -20,11 +22,6 @@ class ProxyRequests:
         matches = findall(r"<td>\d+.\d+.\d+.\d+</td><td>\d+</td>", r.text)
         revised = [m.replace('<td>', '') for m in matches]
         self.sockets = [s[:-5].replace('</td>', ':') for s in revised]
-
-    def try_count_succeeded(self):
-        message = 'Unable to make proxied request... '
-        message += 'Please check the validity of %s' % self.url
-        print(message)
 
     def set_request_data(self, req, socket):
         self.request = req.text
@@ -37,26 +34,16 @@ class ProxyRequests:
         except Exception:
             self.json = {}
 
+    def is_timeout(self, err):
+        if type(err).__name__ not in self.errs:
+            raise err
+
+    def try_count_succeeded(self):
+        raise Exception('Proxy Pool has been emptied')
+
     # recursively try proxy sockets until successful GET
     def get(self):
-        if len(self.sockets) > 0 and self.try_count > 0:
-            current_socket = self.sockets.pop(0)
-            proxies = {
-                'http': 'http://' + current_socket,
-                'https': 'https://' + current_socket
-            }
-            try:
-                request = requests.get(self.url, timeout=3.0, proxies=proxies)
-                self.set_request_data(request, current_socket)
-            except Exception:
-                self.try_count -= 1
-                self.get()
-        else:
-            self.try_count_succeeded()
-
-    # recursively try proxy sockets until successful GET with headers
-    def get_with_headers(self):
-        if len(self.sockets) > 0 and self.try_count > 0:
+        if len(self.sockets) > 0:
             current_socket = self.sockets.pop(0)
             proxies = {
                 'http': 'http://' + current_socket,
@@ -64,18 +51,40 @@ class ProxyRequests:
             }
             try:
                 request = requests.get(
-                    self.url, timeout=3.0,
-                    proxies=proxies, headers=self.headers)
+                    self.url,
+                    timeout=self.timeout,
+                    proxies=proxies)
                 self.set_request_data(request, current_socket)
-            except Exception:
-                self.try_count -= 1
+            except Exception as e:
+                self.is_timeout(e)
+                self.get()
+        else:
+            self.try_count_succeeded()
+
+    # recursively try proxy sockets until successful GET with headers
+    def get_with_headers(self):
+        if len(self.sockets) > 0:
+            current_socket = self.sockets.pop(0)
+            proxies = {
+                'http': 'http://' + current_socket,
+                'https': 'https://' + current_socket
+            }
+            try:
+                request = requests.get(
+                    self.url,
+                    timeout=self.timeout,
+                    proxies=proxies,
+                    headers=self.headers)
+                self.set_request_data(request, current_socket)
+            except Exception as e:
+                self.is_timeout(e)
                 self.get_with_headers()
         else:
             self.try_count_succeeded()
 
     # recursively try proxy sockets until successful POST
     def post(self, data):
-        if len(self.sockets) > 0 and self.try_count > 0:
+        if len(self.sockets) > 0:
             current_socket = self.sockets.pop(0)
             proxies = {
                 'http': 'http://' + current_socket,
@@ -83,17 +92,20 @@ class ProxyRequests:
             }
             try:
                 request = requests.post(
-                    self.url, json=data, timeout=3.0, proxies=proxies)
+                    self.url,
+                    json=data,
+                    timeout=self.timeout,
+                    proxies=proxies)
                 self.set_request_data(request, current_socket)
-            except Exception:
-                self.try_count -= 1
+            except Exception as e:
+                self.is_timeout(e)
                 self.post(data)
         else:
             self.try_count_succeeded()
 
     # recursively try proxy sockets until successful POST with headers
     def post_with_headers(self, data):
-        if len(self.sockets) > 0 and self.try_count > 0:
+        if len(self.sockets) > 0:
             current_socket = self.sockets.pop(0)
             proxies = {
                 'http': 'http://' + current_socket,
@@ -101,18 +113,21 @@ class ProxyRequests:
             }
             try:
                 request = requests.post(
-                    self.url, json=data, timeout=3.0,
-                    headers=self.headers, proxies=proxies)
+                    self.url,
+                    json=data,
+                    timeout=self.timeout,
+                    headers=self.headers,
+                    proxies=proxies)
                 self.set_request_data(request, current_socket)
-            except Exception:
-                self.try_count -= 1
+            except Exception as e:
+                self.is_timeout(e)
                 self.post_with_headers(data)
         else:
             self.try_count_succeeded()
 
     # recursively try proxy sockets until successful POST with file
     def post_file(self):
-        if len(self.sockets) > 0 and self.try_count > 0:
+        if len(self.sockets) > 0:
             current_socket = self.sockets.pop(0)
             proxies = {
                 'http': 'http://' + current_socket,
@@ -120,18 +135,20 @@ class ProxyRequests:
             }
             try:
                 request = requests.post(
-                    self.url, files=self.file_dict,
-                    timeout=3.0, proxies=proxies)
+                    self.url,
+                    files=self.file_dict,
+                    timeout=self.timeout,
+                    proxies=proxies)
                 self.set_request_data(request, current_socket)
-            except Exception:
-                self.try_count -= 1
+            except Exception as e:
+                self.is_timeout(e)
                 self.post_file()
         else:
             self.try_count_succeeded()
 
     # recursively try until successful POST with file and custom headers
     def post_file_with_headers(self):
-        if len(self.sockets) > 0 and self.try_count > 0:
+        if len(self.sockets) > 0:
             current_socket = self.sockets.pop(0)
             proxies = {
                 'http': 'http://' + current_socket,
@@ -139,11 +156,14 @@ class ProxyRequests:
             }
             try:
                 request = requests.post(
-                    self.url, files=self.file_dict, timeout=3.0,
-                    headers=self.headers, proxies=proxies)
+                    self.url,
+                    files=self.file_dict,
+                    timeout=self.timeout,
+                    headers=self.headers,
+                    proxies=proxies)
                 self.set_request_data(request, current_socket)
-            except Exception:
-                self.try_count -= 1
+            except Exception as e:
+                self.is_timeout(e)
                 self.post_file_with_headers()
         else:
             self.try_count_succeeded()
@@ -183,7 +203,7 @@ class ProxyRequestsBasicAuth(ProxyRequests):
 
     # recursively try proxy sockets until successful GET (overrided method)
     def get(self):
-        if len(self.sockets) > 0 and self.try_count > 0:
+        if len(self.sockets) > 0:
             current_socket = self.sockets.pop(0)
             proxies = {
                 'http': 'http://' + current_socket,
@@ -191,18 +211,20 @@ class ProxyRequestsBasicAuth(ProxyRequests):
             }
             try:
                 request = requests.get(
-                    self.url, auth=(self.username, self.password),
-                    timeout=3.0, proxies=proxies)
+                    self.url,
+                    auth=(self.username, self.password),
+                    timeout=self.timeout,
+                    proxies=proxies)
                 self.set_request_data(request, current_socket)
-            except Exception:
-                self.try_count -= 1
+            except Exception as e:
+                self.is_timeout(e)
                 self.get()
         else:
             self.try_count_succeeded()
 
     # recursively try until successful GET with headers (overrided method)
     def get_with_headers(self):
-        if len(self.sockets) > 0 and self.try_count > 0:
+        if len(self.sockets) > 0:
             current_socket = self.sockets.pop(0)
             proxies = {
                 'http': 'http://' + current_socket,
@@ -210,18 +232,21 @@ class ProxyRequestsBasicAuth(ProxyRequests):
             }
             try:
                 request = requests.get(
-                    self.url, auth=(self.username, self.password),
-                    timeout=3.0, proxies=proxies, headers=self.headers)
+                    self.url,
+                    auth=(self.username, self.password),
+                    timeout=self.timeout,
+                    proxies=proxies,
+                    headers=self.headers)
                 self.set_request_data(request, current_socket)
-            except Exception:
-                self.try_count -= 1
+            except Exception as e:
+                self.is_timeout(e)
                 self.get_with_headers()
         else:
             self.try_count_succeeded()
 
     # recursively try proxy sockets until successful POST (overrided method)
     def post(self, data):
-        if len(self.sockets) > 0 and self.try_count > 0:
+        if len(self.sockets) > 0:
             current_socket = self.sockets.pop(0)
             proxies = {
                 'http': 'http://' + current_socket,
@@ -229,18 +254,21 @@ class ProxyRequestsBasicAuth(ProxyRequests):
             }
             try:
                 request = requests.post(
-                    self.url, json=data, auth=(self.username, self.password),
-                    timeout=3.0, proxies=proxies)
+                    self.url,
+                    json=data,
+                    auth=(self.username, self.password),
+                    timeout=self.timeout,
+                    proxies=proxies)
                 self.set_request_data(request, current_socket)
-            except Exception:
-                self.try_count -= 1
+            except Exception as e:
+                self.is_timeout(e)
                 self.post(data)
         else:
             self.try_count_succeeded()
 
     # recursively try until successful POST with headers (overrided method)
     def post_with_headers(self, data):
-        if len(self.sockets) > 0 and self.try_count > 0:
+        if len(self.sockets) > 0:
             current_socket = self.sockets.pop(0)
             proxies = {
                 'http': 'http://' + current_socket,
@@ -248,18 +276,22 @@ class ProxyRequestsBasicAuth(ProxyRequests):
             }
             try:
                 request = requests.post(
-                    self.url, json=data, auth=(self.username, self.password),
-                    timeout=3.0, headers=self.headers, proxies=proxies)
+                    self.url,
+                    json=data,
+                    auth=(self.username, self.password),
+                    timeout=self.timeout,
+                    headers=self.headers,
+                    proxies=proxies)
                 self.set_request_data(request, current_socket)
-            except Exception:
-                self.try_count -= 1
+            except Exception as e:
+                self.is_timeout(e)
                 self.post_with_headers(data)
         else:
             self.try_count_succeeded()
 
     # recursively try proxy sockets until successful POST with file
     def post_file(self):
-        if len(self.sockets) > 0 and self.try_count > 0:
+        if len(self.sockets) > 0:
             current_socket = self.sockets.pop(0)
             proxies = {
                 'http': 'http://' + current_socket,
@@ -267,19 +299,21 @@ class ProxyRequestsBasicAuth(ProxyRequests):
             }
             try:
                 request = requests.post(
-                    self.url, files=self.file_dict,
+                    self.url,
+                    files=self.file_dict,
                     auth=(self.username, self.password),
-                    timeout=3.0, proxies=proxies)
+                    timeout=self.timeout,
+                    proxies=proxies)
                 self.set_request_data(request, current_socket)
-            except Exception:
-                self.try_count -= 1
+            except Exception as e:
+                self.is_timeout(e)
                 self.post_file()
         else:
             self.try_count_succeeded()
 
     # recursively try until successful POST with file and custom headers
     def post_file_with_headers(self):
-        if len(self.sockets) > 0 and self.try_count > 0:
+        if len(self.sockets) > 0:
             current_socket = self.sockets.pop(0)
             proxies = {
                 'http': 'http://' + current_socket,
@@ -287,12 +321,15 @@ class ProxyRequestsBasicAuth(ProxyRequests):
             }
             try:
                 request = requests.post(
-                    self.url, files=self.file_dict,
-                    auth=(self.username, self.password), timeout=3.0,
-                    headers=self.headers, proxies=proxies)
+                    self.url,
+                    files=self.file_dict,
+                    auth=(self.username, self.password),
+                    timeout=self.timeout,
+                    headers=self.headers,
+                    proxies=proxies)
                 self.set_request_data(request, current_socket)
-            except Exception:
-                self.try_count -= 1
+            except Exception as e:
+                self.is_timeout(e)
                 self.post_file_with_headers()
         else:
             self.try_count_succeeded()
